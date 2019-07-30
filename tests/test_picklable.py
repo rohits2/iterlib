517821646d7eed98d3e54b1dc7e69bee99feb96e
Rohit Singh <singhrohit2@hotmail.com>
2019-07-29 22:22:27 -0600

    Add a test to verify that unpickable objects work with thread distributors

---
+++ tests/test_picklable.py
@@ -0,0 +1,28 @@
+import pytest
+import iterlib
+import random
+
+class Unpicklable:
+    def __init__(self, q):
+        self.func1 = lambda x: q*x**2
+        self.func2 = lambda x: q*x
+        self.q = q
+
+def test_thread_map_pickle():
+    TEST_VAL = 5
+    objs = [Unpicklable(random.randint(0, 1000)) for _ in range(100)]
+    identity_map_objs = iterlib.thread_map(lambda x: x, objs)
+    for orig, mapped in zip(objs, identity_map_objs):
+        assert orig.q == mapped.q
+        assert orig.func1(TEST_VAL) == mapped.func1(TEST_VAL)
+        assert orig.func2(TEST_VAL) == mapped.func2(TEST_VAL)
+
+def test_thread_preload_pickle():
+    TEST_VAL = 5
+    objs = [Unpicklable(random.randint(0, 1000)) for _ in range(100)]
+    identity_map_objs = iterlib.thread_preload(objs)
+    for orig, mapped in zip(objs, identity_map_objs):
+        assert orig.q == mapped.q
+        assert orig.func1(TEST_VAL) == mapped.func1(TEST_VAL)
+        assert orig.func2(TEST_VAL) == mapped.func2(TEST_VAL)
+
