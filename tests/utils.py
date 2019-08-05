0bf1e00b4ce1b39865dde8dac5682b758d6f0550
Rohit Singh <singhrohit2@hotmail.com>
2019-08-04 21:49:52 -0600

    Extend timeout

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,7 @@
 from time import sleep
 def wait_until_shutdown(itr):
     i = 0
-    while i < 20 and not itr.is_shutdown():
-        print(itr.is_shutdown())
+    while i < 200 and not itr.is_shutdown():
         sleep(0.5)
         i += 1
     return itr.is_shutdown()
